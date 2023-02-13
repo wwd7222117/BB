e9fdd04c4933d4b2b13fd077bd47d381e0d8352f
unknown <wwd7222117@gmail.com>
2023-02-13 22:54:19 +0800

    init

---
+++ Game/src/Mob.cpp
@@ -0,0 +1,168 @@
+// MIT License
+// 
+// Copyright(c) 2020 Arthur Bacon and Kevin Dill
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this softwareand associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright noticeand this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "Mob.h"
+
+#include "Constants.h"
+#include "Game.h"
+
+#include <algorithm>
+#include <vector>
+
+
+Mob::Mob(const iEntityStats& stats, const Vec2& pos, bool isNorth)
+    : Entity(stats, pos, isNorth)
+    , m_pWaypoint(NULL)
+{
+    assert(dynamic_cast<const iEntityStats_Mob*>(&stats) != NULL);
+}
+
+void Mob::tick(float deltaTSec)
+{
+    // Tick the entity first.  This will pick our target, and attack it if it's in range.
+    Entity::tick(deltaTSec);
+
+    // if our target isn't in range, move towards it.
+    if (!targetInRange())
+    {
+        move(deltaTSec);
+    }
+}
+
+void Mob::move(float deltaTSec)
+{
+    // If we have a target and it's on the same side of the river, we move towards it.
+    //  Otherwise, we move toward the bridge.
+    bool bMoveToTarget = false;
+    if (!!m_pTarget)
+    {    
+        bool imTop = m_Pos.y < (GAME_GRID_HEIGHT / 2);
+        bool otherTop = m_pTarget->getPosition().y < (GAME_GRID_HEIGHT / 2);
+
+        if (imTop == otherTop)
+        {
+            bMoveToTarget = true;
+        }
+    }
+
+    Vec2 destPos;
+    if (bMoveToTarget)
+    { 
+        m_pWaypoint = NULL;
+        destPos = m_pTarget->getPosition();
+    }
+    else
+    {
+        if (!m_pWaypoint)
+        {
+            m_pWaypoint = pickWaypoint();
+        }
+        destPos = m_pWaypoint ? *m_pWaypoint : m_Pos;
+    }
+
+    // Actually do the moving
+    Vec2 moveVec = destPos - m_Pos;
+    float distRemaining = moveVec.normalize();
+    float moveDist = m_Stats.getSpeed() * deltaTSec;
+
+    // if we're moving to m_pTarget, don't move into it
+    if (bMoveToTarget)
+    {
+        assert(m_pTarget);
+        distRemaining -= (m_Stats.getSize() + m_pTarget->getStats().getSize()) / 2.f;
+        distRemaining = std::max(0.f, distRemaining);
+    }
+
+    if (moveDist <= distRemaining)
+    {
+        m_Pos += moveVec * moveDist;
+    }
+    else
+    {
+        m_Pos += moveVec * distRemaining;
+
+        // if the destination was a waypoint, find the next one and continue movement
+        if (m_pWaypoint)
+        {
+            m_pWaypoint = pickWaypoint();
+            destPos = m_pWaypoint ? *m_pWaypoint : m_Pos;
+            moveVec = destPos - m_Pos;
+            moveVec.normalize();
+            m_Pos += moveVec * distRemaining;
+        }
+    }
+
+    // PROJECT 1: This is where your collision code will be called from
+    Mob* otherMob = checkCollision();
+    if (otherMob) {
+        processCollision(otherMob, deltaTSec);
+    }
+}
+
+const Vec2* Mob::pickWaypoint()
+{
+    float smallestDistSq = FLT_MAX;
+    const Vec2* pClosest = NULL;
+
+    for (const Vec2& pt : Game::get().getWaypoints())
+    {
+        // Filter out any waypoints that are behind (or barely in front of) us.
+        // NOTE: (0, 0) is the top left corner of the screen
+        float yOffset = pt.y - m_Pos.y;
+        if ((m_bNorth && (yOffset < 1.f)) ||
+            (!m_bNorth && (yOffset > -1.f)))
+        {
+            continue;
+        }
+
+        float distSq = m_Pos.distSqr(pt);
+        if (distSq < smallestDistSq) {
+            smallestDistSq = distSq;
+            pClosest = &pt;
+        }
+    }
+
+    return pClosest;
+}
+
+// PROJECT 1: 
+//  1) return a vector of mobs that we're colliding with
+//  2) handle collision with towers & river 
+Mob* Mob::checkCollision() 
+{
+    //for (const Mob* pOtherMob : Game::get().getMobs())
+    //{
+    //    if (this == pOtherMob) 
+    //    {
+    //        continue;
+    //    }
+
+    //    // PROJECT 1: YOUR CODE CHECKING FOR A COLLISION GOES HERE
+    //}
+    return NULL;
+}
+
+void Mob::processCollision(Mob* otherMob, float deltaTSec) 
+{
+    // PROJECT 1: YOUR COLLISION HANDLING CODE GOES HERE
+}
+
